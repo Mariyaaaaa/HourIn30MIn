bc863c552e14213d4bc710830898697c188e7fba
Mariya <mariqvalkova@gmail.com>
2018-05-23 15:50:31 +0300

    CalculateHourIn30Min

---
+++ StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Start
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var hour = int.Parse(Console.ReadLine());
+            var minutes = int.Parse(Console.ReadLine());
+            int newMinutes = minutes + 30;
+            
+
+
+            if (newMinutes >= 60 )
+            {
+                hour= hour + 1;
+                newMinutes = newMinutes - 60;
+            }
+            if (hour >= 24)
+            {
+                hour = 0;
+            }
+            if (newMinutes < 10)
+            {
+                Console.WriteLine($"{hour}:{newMinutes:D2}");
+            }
+            else
+            {
+                 Console.WriteLine($"{hour}:{newMinutes}");
+
+            }
+
+        }
+    }
+}
